9c4fb50afb630f9fd384c0034d2db10c54abe6ba
kah78787 <thrpinkrain777@gmail.com>
2025-05-31 04:08:20 +0400

    add sign up to AuthService

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,6 +3,8 @@
 
 namespace App\Services;
 
+use App\Http\Requests\SignUpRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -20,5 +22,31 @@ class AuthService
             'user' => $user,
             'token' => $user->createToken('api')->plainTextToken
         ];
+    }
+
+    public function signUpUserIfNotExist(string $name , string $email , string $password ,string $user_type )
+    {
+        $checkEmailIfExist = User::checkEmailIfExist($email);
+
+        if(!$checkEmailIfExist) {
+            $user = User::Create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+
+            if($user_type == 'doctor') {
+                $user->doctor()->create([]);
+            } else {
+                $user->guardian()->create([]);
+            }
+
+            return [
+                'user' => $user,
+                'token' => $user->createToken('api')->plainTextToken,
+            ];
+        }else {
+            return null;
+        }
     }
 }
