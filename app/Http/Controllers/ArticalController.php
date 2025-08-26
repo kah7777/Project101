f30231013ac356655bab80fc35941fd2e6aa17e6
Reem <reemahmadn22@gmail.com>
2025-08-26 14:19:59 +0300

    add articals

---
+++ app/Http/Controllers/ArticalController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
+use App\Services\ApiResponseService;
+use Illuminate\Http\Request;
+
+class ArticalController extends Controller
+{
+
+       public function store(StoreArticleRequest $request)
+    {
+        try {
+            $article = Article::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'content' => $request->content,
+            ]);
+
+            if ($request->hasFile('image')) {
+                $article->addMediaFromRequest('image')->toMediaCollection('articles');
+            }
+
+            return ApiResponseService::success([
+                'article' => ArticleResource::make($article)
+            ], 'Article created successfully');
+        } catch (\Exception $e) {
+            return ApiResponseService::error(
+                'Failed to create article: ' . $e->getMessage(),
+                500
+            );
+        }
+    }
+
+}
